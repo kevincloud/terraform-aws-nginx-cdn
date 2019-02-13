e8a4da9efd7fe8e74878f3b9900bfe597abc7f5b
Kevin Cochran <kcochran@hashicorp.com>
2019-02-13 15:30:23 -0500

    add splat to output

--- modules/nginx-euwest/outputs.tf
+++ modules/nginx-euwest/outputs.tf
@@ -3,5 +3,5 @@ output "ip_address" {
 }
 
 output "host_name" {
-    value = "${element(compact(concat(list(var.public-dns), aws_instance.nginx_instance.public_dns)), 0)}"
+    value = "${element(compact(concat(list(var.public-dns), aws_instance.nginx_instance.*.public_dns)), 0)}"
 }
