ad1c16c0cbbfe00a7bc1215bf5c5c7d36baad49e
Kevin Cochran <kcochran@hashicorp.com>
2019-04-04 01:22:40 -0400

    grant traffic between vpcs

--- modules/nginx-euwest/main.tf
+++ modules/nginx-euwest/main.tf
@@ -45,6 +45,20 @@ resource "aws_security_group" "nginx-sg" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["10.0.0.0/8"]
+    }
+
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["10.0.0.0/8"]
+    }
+
     egress {
         from_port = 0
         to_port = 0
