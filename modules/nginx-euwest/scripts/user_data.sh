f7a89aa7329a268158d1533c5e374c2476e4ba28
Kevin Cochran <kcochran@hashicorp.com>
2019-04-08 10:21:46 -0400

    change git url

--- modules/nginx-euwest/scripts/user_data.sh
+++ modules/nginx-euwest/scripts/user_data.sh
@@ -7,7 +7,7 @@ apt install -y nginx
 mkdir -p /data/nginx/cache
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
-wget https://raw.githubusercontent.com/kevincloud/terraform-aws-nginx/master/scripts/content_server
+wget https://raw.githubusercontent.com/kevincloud/terraform-aws-nginx-cdn/master/modules/nginx-euwest/scripts/content_server
 sed -i -e 's/{HOSTNAME}/${web_server_address}/g' ./content_server
 cp ./content_server /etc/nginx/sites-available/
 ln -s /etc/nginx/sites-available/content_server /etc/nginx/sites-enabled/content_server
